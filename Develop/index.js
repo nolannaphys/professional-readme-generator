3dc8dc09a95ca6bcc253345883a41b412f37da4a
Nolan <nolannaphys@gmail.com>
2023-05-16 20:11:00 -0400

    finished questions in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -11,8 +11,48 @@ const questions = [        {
 },
 {
     type: 'input',
-    message: 'How would you describe your project?',
-    name: 'Description'
+    message: 'Will your readme need to have a table of contents?',
+    name: 'Table of Contents'
+},
+{
+    type: 'input',
+    message: 'How do you install your project?',
+    name: 'Installation'
+},
+{
+    type: 'input',
+    message: 'How do you use your project?',
+    name: 'Usage'
+},
+{
+    type: 'input',
+    message: 'What are the credits of your project?',
+    name: 'Credits'
+},
+{
+    type: 'input',
+    message: 'What license did you use?',
+    name: 'License'
+},
+{
+    type: 'input',
+    message: 'What badges did you use for your project?',
+    name: 'Badges'
+},
+{
+    type: 'input',
+    message: 'What features does your project have?',
+    name: 'Features'
+},
+{
+    type: 'input',
+    message: 'How can others contribute to your project?',
+    name: 'How to Contribute'
+},
+{
+    type: 'input',
+    message: 'What are the tests for your project?',
+    name: 'Tests'
 }];
 
 // TODO: Create a function to write README file
