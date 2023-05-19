b88eeecf510a0b4d88a1d8b362379873a242f567
Nolan <nolannaphys@gmail.com>
2023-05-18 22:21:42 -0400

    why is everything undefined?

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -35,14 +35,14 @@ function renderLicenseLink(license) {
   }
   else{
       return '';
-  }
-}
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   return renderLicenseBadge(license) + renderLicenseLink(license)
-}
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown({title, description, installation, usage, credits, license, features, contribution, test}) {
@@ -76,6 +76,8 @@ ${contribution}
 
 ## Test
 ${test}
-`;}
+`;
+};
+
 
 module.exports = generateMarkdown;
